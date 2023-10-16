4c7786529c801822f05e796d13017a111714ea7f
Monasef <monasef77@gmail.com>
2023-10-16 20:09:30 +0100

    setup projects, add styled-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
+import styled from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles';
+import Button from './ui/Button';
+import Heading from './ui/Heading';
+import Row from './ui/Row';
+
 function App() {
-  return <div>App</div>;
+  const StyledApp = styled.div`
+    padding: 3rem;
+    background-color: var(--color-brand-200);
+  `;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Row type="vertical">
+          <Row type="horizontal">
+            <Heading as="h1">Heading H1</Heading>
+            <Button>Click</Button>
+            <Button variation="danger" size="large">
+              Click here !!
+            </Button>
+          </Row>
+          <Row type="vertical">
+            <Heading as="h2">Heading h2</Heading>
+            <Heading as="h3">Heading h3</Heading>
+          </Row>
+        </Row>
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
---
+++ src/ui/Row.jsx
@@ -0,0 +1,17 @@
+import styled, { css } from 'styled-components';
+
+const Row = styled.div`
+  display: flex;
+  ${(props) =>
+    props.type === 'horizontal' &&
+    css`
+      justify-content: space-between;
+    `};
+  ${(props) =>
+    props.type === 'vertical' &&
+    css`
+      flex-direction: column;
+      gap: 1.4rem;
+    `};
+`;
+export default Row;
